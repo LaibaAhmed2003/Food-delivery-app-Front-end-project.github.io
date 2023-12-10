8d6b80b30be5a6deb0ddd4db61beebf42e4db795
SIDRA <sdrrzq@gmail.com>
2023-12-10 17:49:05 +0500

    changes

--- src/Router.jsx
+++ src/Router.jsx
@@ -3,8 +3,8 @@ import Navbar from "./Navbar";
 import Home from "./pages/Home";
 import Burgers from "./pages/Burgers";
 import Signup from "./pages/Signup";
-
 import Signin from "./pages/Signin";
+import Search from "./pages/Search";
 
 
 const Router = () => {
@@ -16,9 +16,7 @@ const Router = () => {
 
         <Route path="/signup" element={<Signup />} />
         <Route path="/signin" element={<Signin />} />
-
-
-
+        <Route path="/search" element={<Search />} />
         <Route path="/burgers" element={<Burgers />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
---
+++ src/pages/Search.jsx
@@ -0,0 +1,47 @@
+import { useState, useEffect } from 'react';
+import FoodCard from '../components/foodcard';
+
+
+const Search = () => {
+  const [searchInput, setSearchInput] = useState('');
+ 
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  
+  const filteredFood = Food.filter(item =>
+    item.title && item.title.toLowerCase().includes(searchInput.toLowerCase())
+  );
+  
+
+  return (
+    <div className='main-contain'>
+      <h1 className='Food-heading'>FOOD_ITEMS</h1>
+      <div>
+        <input
+          type="text"
+          placeholder="Search Food"
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
+        />
+      </div>
+      <div className='food-page'>
+        {filteredFood.map((item, index) => (
+          <FoodCard
+            key={index}
+            name={item.title}
+            price={item.price}
+            category={item.category}
+            image={item.image}
+            id={item.id}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+
+export default Search;
