a3d2dea45a6ff019a5a8347ce33b1c1250d5c892
4Hadry <chammar234@gmail.com>
2023-12-10 17:24:24 +0500

    food Listing Done

--- src/components/foodcard.jsx
+++ src/components/foodcard.jsx
@@ -1,24 +1,31 @@
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
-const ProductCard = ({ name, price, image, discription, id}) => {
+const ProductCard = ({ name, price, img, dec, id }) => {
   return (
-    <div className="food-card">
-      <div className="image-row">
-      <img className="food-imge" src={image} alt="" />
-      </div>
-      <h6 className="food-discription">{discription}</h6>
-      <h2 className="food-title">{name}</h2>
-      <h5 className="food-price">{price}</h5>
-
-      <Link 
-      to={`/fooddetail/${id}`}
-      className="btn btn-primary food-btn"
-      >
-        ShowDetails
-        </Link>
+    <div className="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
+      <a href="#">
+        <img
+          src={img}
+          alt="Food"
+          className="h-80 w-72 object-cover rounded-t-xl"
+        />
+        <div className="px-4 py-3 w-72">
+          <span className="text-gray-400 mr-3 uppercase text-xs">KFC</span>
+          <p className="text-lg font-bold text-black truncate block capitalize">
+            {name}
+          </p>
+          <div className="flex items-center">
+            <p className="text-lg font-semibold text-black cursor-auto my-3">
+              $149
+            </p>
+            <del>
+              <p className="text-sm text-gray-600 cursor-auto ml-2">$199</p>
+            </del>
+          </div>
+        </div>
+      </a>
     </div>
   );
-
 };
 
 export default ProductCard;
--- src/pages/Burgers.jsx
+++ src/pages/Burgers.jsx
@@ -1,11 +1,11 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-import FoodCard from '../components/foodcard';
+import { useState, useEffect } from "react";
+import axios from "axios";
+import FoodCard from "../components/foodcard";
 
 const Burgers = () => {
   const [Food, setFood] = useState([]);
-  const [searchInput, setSearchInput] = useState('');
-  console.log(Food);
+  const [searchInput, setSearchInput] = useState("");
+  // console.log(Food[0].name);
 
   useEffect(() => {
     getData();
@@ -13,26 +13,28 @@ const Burgers = () => {
 
   const getData = async () => {
     try {
-      const resp = await axios.get('http://localhost:3000/foods/get');
+      const resp = await axios.get("http://localhost:3000/foods/get");
+      // console.log(resp);
       const responseData = resp.data.myData;
+      console.log(responseData);
 
-      if (Array.isArray(responseData)) {
-        setFood(responseData);
-      } else {
-        console.error("Invalid data format. Expected an array.");
-      }
+      setFood(responseData);
+      // if (Array.isArray(responseData)) {
+      // } else {
+      //   console.error("Invalid data format. Expected an array.");
+      // }
     } catch (e) {
       console.error("Error fetching data:", e);
     }
   };
-  const filteredFood = Food.filter(item =>
-    item.title && item.title.toLowerCase().includes(searchInput.toLowerCase())
-  );
-  
+  // const filteredFood = Food.filter(
+  //   (item) =>
+  //     item.title && item.title.toLowerCase().includes(searchInput.toLowerCase())
+  // );
 
   return (
-    <div className='main-contain'>
-      <h1 className='Food-heading'>FOOD_ITEMS</h1>
+    <div className="main-contain">
+      <h1 className="Food-heading">FOOD_ITEMS</h1>
       <div>
         <input
           type="text"
@@ -40,15 +42,17 @@ const Burgers = () => {
           value={searchInput}
           onChange={(e) => setSearchInput(e.target.value)}
         />
+        {/* <h1>{Food[0].name}</h1> */}
       </div>
-      <div className='food-page'>
-        {filteredFood.map((item, index) => (
+      <div className="food-page w-fit mx-auto grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 justify-items-center justify-center gap-y-20 gap-x-14 mt-10 mb-5">
+        {Food.map((item, index) => (
           <FoodCard
             key={index}
-            name={item.title}
+            name={item.name}
             price={item.price}
-            category={item.category}
-            image={item.image}
+            category={item.CategoryName}
+            img={item.img}
+            dec={item.description}
             id={item.id}
           />
         ))}
@@ -56,6 +60,5 @@ const Burgers = () => {
     </div>
   );
 };
-
 
 export default Burgers;
