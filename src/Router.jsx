c96f37cdc1b06b99f3101f3cda038512288573b5
SIDRA <sdrrzq@gmail.com>
2023-12-10 12:39:47 +0500

    searching

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./Navbar";
 import Home from "./pages/Home";
+import Search from "./pages/Search";
 
 const Router = () => {
   return (
@@ -8,6 +9,10 @@ const Router = () => {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+     
+
+      <Route path="/search" element={<Search />} />
+
       </Routes>
     </BrowserRouter>
   );
