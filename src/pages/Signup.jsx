0767c6c50d28740375816a5c1a62a4859fd33b42
Ramsha Shahzad <ramshashahzad113@gmail.com>
2023-12-10 15:53:10 +0500

    Signup Page UI done

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,62 +1,81 @@
 import React from "react";
-import { Fragment } from "react";
-import { Form, Feild, ErrorMessage } from "formik";
+// import { Fragment } from "react";
+// import { Formik, Form, Field, ErrorMessage } from "formik";
 
 const Signup = () => {
   return (
-    <Fragment>
-      <div className="signup-conatiner">
-        <div className="col-md-12 text-center">
-          <h1>SignUp</h1>
-          <Form>
-            <div className="col-md-12">
-              <Feild
+    <>
+      <div className="h-300 w-500 flex items-center justify-center">
+        <div className="bg-white p-16 rounded shadow-2xl w-2/3">
+          <h2 className="text-3xl font-bold mb-10 text-gray-800">Sign up</h2>
+          <form className="space-y-5">
+            <div>
+              <label className="block mb-1 font-bold text-gray-500">
+                {" "}
+                First Name
+              </label>
+              <input
                 type="text"
-                name="name"
-                palceholder="First Name"
-                className="form-control"
+                placeholder=""
+                className="w-full border-2 border-gray-200 p-3 rounded outline-none focus:border-purple-500"
               />
-              <p className="text-danger">
-                <ErrorMessage name="name" />
-              </p>
             </div>
-            <div className="col-md-12">
-              <Feild
+            <div>
+              <label className="block mb-1 font-bold text-gray-500">
+                {" "}
+                Last Name
+              </label>
+              <input
                 type="text"
-                name="email"
-                palceholder="Email"
-                className="form-control"
+                placeholder=""
+                className="w-full border-2 border-gray-200 p-3 rounded outline-none focus:border-purple-500"
               />
-              <p className="text-danger">
-                <ErrorMessage name="email" />
-              </p>
             </div>
-            <div className="col-md-12">
-              <Feild
+            <div>
+              <label className="block mb-1 font-bold text-gray-500">
+                Email
+              </label>
+              <input
                 type="text"
-                name="contactnumber"
-                palceholder="Phone Number"
-                className="form-control"
+                placeholder=""
+                className="w-full border-2 border-gray-200 p-3 rounded outline-none focus:border-purple-500"
               />
-              <p className="text-danger">
-                <ErrorMessage name="contactnumber" />
-              </p>
             </div>
-            <div className="col-md-12">
-              <Feild
+            <div>
+              <label className="block mb-1 font-bold text-gray-500">
+                Mobile
+              </label>
+              <input
+                type="text"
+                placeholder=""
+                className="w-full border-2 border-gray-200 p-3 rounded outline-none focus:border-purple-500"
+              />
+            </div>
+
+            <div>
+              <label className="block mb-1 font-bold text-gray-500">
+                Password
+              </label>
+              <input
                 type="password"
-                name="password"
-                palceholder="First Your Password"
-                className="form-control"
+                placeholder=""
+                className="w-full border-2 border-gray-200 p-3 rounded outline-none focus:border-purple-500"
               />
-              <p className="text-danger">
-                <ErrorMessage name="password" />
-              </p>
             </div>
-          </Form>
+
+            <button className="block w-full bg-red-400 hover:bg-red-300 p-4 rounded text-white-900 hover:text-red-800 transition duration-300">
+              Create Account
+            </button>
+            <p class="mt-2 text-black-500 text-xs">
+              Already have an account,{" "}
+              <a href="#" class="text-black-700 font-bold">
+                LogIn
+              </a>
+            </p>
+          </form>
         </div>
       </div>
-    </Fragment>
+    </>
   );
 };
 
