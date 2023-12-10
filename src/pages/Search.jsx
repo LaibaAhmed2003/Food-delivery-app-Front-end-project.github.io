0d163a772092e3129e500c00dd22a9c494865534
SIDRA <sdrrzq@gmail.com>
2023-12-10 13:56:40 +0500

    searchcss

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -11,11 +11,37 @@ const SearchBar = ({ onSearch }) => {
     <div>
       <input
         type="text"
-        placeholder="Search..."
+        placeholder="Search ..."
         value={searchInput}
         onChange={(e) => setSearchInput(e.target.value)}
+        style={{
+          width: '100%',
+          maxwidth: '400px',
+          height: '40px',
+          margin: '0 auto',
+          padding: '0 10px',
+          border: '1px solid #ccc',
+          borderRadius: '4px',
+        }}
       />
-      <button onClick={handleSearch}>Search</button>
+      <button onClick={handleSearch} style={{
+        width: '40px',
+        height: '40px',
+        margin: '0 0 0 -40px',
+        border: 'none',
+        backgroundColor: '#fff',
+        cursor: 'pointer',
+      }}>
+        <span style={{
+          content: '',
+          width: '16px',
+          height: '16px',
+          margin: '12px auto',
+          backgroundImage: 'url("https://i.imgur.com/search-icon.png")',
+          backgroundSize: 'contain',
+          backgroundRepeat: 'no-repeat',
+        }} />
+      </button>
     </div>
   );
 };
