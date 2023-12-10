a6775b96eb433c301e7a115f7a0349b2793b8985
4Hadry <chammar234@gmail.com>
2023-12-10 16:20:33 +0500

    signUp and signin Ui done

--- src/Router.jsx
+++ src/Router.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./Navbar";
 import Home from "./pages/Home";
 import Burgers from "./pages/Burgers";
+import Signup from "./pages/Signup";
+import Signin from "./pages/Signin";
 
 const Router = () => {
   return (
@@ -9,7 +11,9 @@ const Router = () => {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+
         <Route path="/burgers" element={<Burgers />} />
       </Routes>
     </BrowserRouter>
