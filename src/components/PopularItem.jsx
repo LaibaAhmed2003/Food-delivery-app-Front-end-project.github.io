f9adfcd22ddaa2469593158584dab1e9ba26d734
4Hadry <chammar234@gmail.com>
2023-12-11 17:39:05 +0500

    Popular food section done in Home

--- src/components/PopularItem.jsx
+++ src/components/PopularItem.jsx
@@ -1,19 +1,17 @@
-import { Link } from "react-router-dom";
+// import React from 'react'
 
-const ProductCard = ({ name, price, img, dec, id }) => {
+const PopularItem = ({ name, price, img }) => {
   return (
     <div className="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
-      <Link
-        to={`/burger-detail/${id}`}
-        className="btn btn-primary product-btn"
-      ></Link>
       <img
         src={img}
         alt="Food"
         className="h-80 w-72 object-cover rounded-t-xl"
       />
       <div className="px-4 py-3 w-72">
-        <span className="text-gray-400 mr-3 uppercase text-xs">Foodfest</span>
+        <span className="text-gray-400 mr-3 uppercase text-xs">
+          Batter then KFC
+        </span>
         <p className="text-lg font-bold text-black truncate block capitalize">
           {name}
         </p>
@@ -25,9 +23,12 @@ const ProductCard = ({ name, price, img, dec, id }) => {
             <p className="text-sm text-gray-600 cursor-auto ml-2">$199</p>
           </del>
         </div>
+        <button className="h-7 p-1  text-center font-semibold rounded-md bg-indigo-600 hover:bg-indigo-500 text-white">
+          +
+        </button>
       </div>
     </div>
   );
 };
 
-export default ProductCard;
+export default PopularItem;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,24 @@
+import axios from "axios";
+import PopularItem from "../components/PopularItem";
+import { useEffect, useState } from "react";
+import { NavLink } from "react-router-dom";
+
 const Home = () => {
+  const [food, setFood] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get("http://localhost:3000/foods/popular");
+      const data = response.data.myData;
+      console.log(data);
+      setFood(data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
   return (
     <>
       <div className="fit">
@@ -6,6 +26,31 @@ const Home = () => {
           Order Now
         </a>
       </div>
+      <div className="text-center p-10">
+        <h1 className="font-bold text-4xl mb-4">Popular Food</h1>
+        {/* <h1 className="text-3xl"></h1> */}
+      </div>
+
+      <section className="w-fit mx-auto grid grid-cols-1 lg:grid-cols-4 md:grid-cols-2 justify-items-center justify-center gap-y-20 gap-x-14 mt-10 mb-5">
+        {/* <PopularItem/> */}
+        {food.map((item, index) => (
+          <PopularItem
+            key={index}
+            name={item.name}
+            price={item.price}
+            category={item.CategoryName}
+            img={item.img}
+            dec={item.description}
+            id={item.id}
+          />
+        ))}
+      </section>
+      <div className="w-full flex justify-center">
+        <NavLink to="/burgers">
+          <a className=" text-blue-700 ">Show More</a>
+        </NavLink>
+      </div>
+      <br />
     </>
   );
 };
