674f5f2611ec630d31bb1012ad820c349224265b
Laiba <laiba.sabahat9@gmail.com>
2023-12-14 13:27:41 +0500

    css added

--- src/pages/BurgerDetail.jsx
+++ src/pages/BurgerDetail.jsx
@@ -48,19 +48,19 @@ const FoodDetail = () => {
   return (
     <div className="food-detail food-page w-fit mx-auto grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 justify-items-center justify-center gap-y-20 gap-x-14 mt-10 mb-5 ">
       <img src={foodDetail.img} alt={foodDetail.name} />
-      <h2>{foodDetail.name}</h2>
-      <p>Price: ${foodDetail.price}</p>
-
-      <p>{foodDetail.description}</p>
-
-      <Link to="/cart">
-        <button
-          className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
-          onClick={() => cartHandler(item)}
-        >
-          add to card
-        </button>
-      </Link>
+      <div className="detail">
+        <h2 className="py-4">{foodDetail.name}</h2>
+        <p className="py-4">Price: {foodDetail.price}Rs</p>
+        <p className="py-4">{foodDetail.description}</p>
+        <Link to="/cart">
+          <button
+            className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+            onClick={() => cartHandler(item)}
+          >
+            add to card
+          </button>
+        </Link>
+      </div>
     </div>
   );
 };
