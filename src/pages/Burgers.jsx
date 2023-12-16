f0586310b2cd02542e7309bfa368d048c3f92fac
SIDRA <sdrrzq@gmail.com>
2023-12-16 19:24:49 +0500

    categorydone

--- src/pages/Burgers.jsx
+++ src/pages/Burgers.jsx
@@ -1,57 +1,88 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import FoodCard from "../components/foodcard";
-import Category from "../components/categories";
+import CategoryItems from "./categoryItems";
 import { useParams } from "react-router-dom";
-//import CategoryItems from "./categoryItems";
 
 const Burgers = () => {
-
-  const [Food, setFood] = useState([]);
+  const [food, setFood] = useState([]);
   const [searchInput, setSearchInput] = useState("");
   const { categoryName } = useParams();
+  const [categories, setCategories] = useState([]);
   const [selectedCategory, setSelectedCategory] = useState(categoryName);
-  console.log(selectedCategory);
 
   useEffect(() => {
-    getData();
-  }, [selectedCategory]); // Add selectedCategory as a dependency to refetch data when category changes
-
-  const handleSelectCategory = (categoryId) => {
-    setSelectedCategory(categoryId);
-  };
-
-  const getData = async () => {
-    try {
-      let url = "http://localhost:3000/foods/get";
-
-      // If a category is selected, modify the URL to fetch foods of that category
-      if (selectedCategory) {
-        url += `/category/${selectedCategory}`;
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("http://localhost:3000/foods/get");
+        setFood(response.data.myData);
+      } catch (error) {
+        console.error("Error fetching food data:", error);
       }
+    };
 
-      const resp = await axios.get(url);
-      const responseData = resp.data.myData;
-      setFood(responseData);
-    } catch (e) {
-      console.error("Error fetching data:", e);
-    }
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        const response = await axios.get("http://localhost:3000/foods/category");
+        setCategories(response.data.myData);
+      } catch (error) {
+        console.error("Error fetching categories:", error);
+      }
+    };
+
+    fetchCategories();
+  }, []);
+
+  const handleSelectCategory = (categoryName) => {
+    setSelectedCategory(categoryName);
   };
 
   const filterFood = () => {
-    return Food.filter(
-      (item) =>
-        item.name &&
-        item.name.toLowerCase().includes(searchInput.toLowerCase())
-    );
+    if (selectedCategory === "all") {
+      return food;
+    } else {
+      return food.filter((item) => item.CategoryName === selectedCategory);
+    }
   };
-
-  const filteredFood = filterFood();
 
   return (
     <div className="main-contain">
-      <Category onSelectCategory={handleSelectCategory} />
-      <div className="" style={{ textAlign: "center", padding: "24px" }}>
+      <div className="grid grid-cols-1 gap-5">
+        <div className="bg-gray-200 p-4 border rounded">
+          <h2 className="text-lg font-bold mb-4">Categories</h2>
+          <ul>
+            <li key="all" className="mb-2">
+              <button
+                className="text-black-500 hover:text-blue-500 transition duration-300"
+                onClick={() => handleSelectCategory("all")}
+                style={{ textDecoration: "none", cursor: "pointer" }}
+              >
+                All 
+              </button>
+            </li>
+            {categories.map((category) => (
+              <li key={category._id} className="mb-2">
+                <button
+                  className="text-black-500 hover:text-blue-500 transition duration-300"
+                  onClick={() => handleSelectCategory(category.CategoryName)}
+                  style={{ textDecoration: "none", cursor: "pointer" }}
+                >
+                  {category.CategoryName}
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+
+      <div
+        className=""
+        style={{ textAlign: "center", padding: "24px", marginTop: "20px" }}
+      >
         <input
           className=""
           style={{
@@ -66,8 +97,9 @@ const Burgers = () => {
           onChange={(e) => setSearchInput(e.target.value)}
         />
       </div>
+
       <div className="food-page w-fit mx-auto grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 justify-items-center justify-center gap-y-20 gap-x-14 mt-10 mb-5 ">
-        {filteredFood.map((item) => (
+        {filterFood().map((item) => (
           <div key={item._id}>
             <FoodCard
               name={item.name}
